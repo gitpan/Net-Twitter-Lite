069f30710e8d4befef62cc0e460f674342d14f63
Marc Mims <mmims@cpan.org>
2010-05-21 14:00:18 +0000

    Import of MMIMS/Net-Twitter-Lite-0.10000 from CPAN.
    gitpan-cpan-distribution: Net-Twitter-Lite
    gitpan-cpan-version:      0.10000
    gitpan-cpan-path:         MMIMS/Net-Twitter-Lite-0.10000.tar.gz
    gitpan-cpan-author:       MMIMS
    gitpan-cpan-maturity:     released

--- lib/Net/Twitter/Lite.pm
+++ lib/Net/Twitter/Lite.pm
@@ -3,7 +3,7 @@ use 5.005;
 use warnings;
 use strict;
 
-our $VERSION = '0.09002_03';
+our $VERSION = '0.10000';
 $VERSION = eval { $VERSION };
 
 use Carp;
@@ -1560,7 +1560,7 @@ Net::Twitter::Lite - A perl interface to the Twitter API
 
 =head1 VERSION
 
-This document describes Net::Twitter::Lite version 0.09002_03
+This document describes Net::Twitter::Lite version 0.10000
 
 =head1 SYNOPSIS
 
@@ -1845,7 +1845,7 @@ C<useragent_class>, above.  It defaults to {} (an empty HASH ref).
 =item useragent
 
 The value for C<User-Agent> HTTP header.  It defaults to
-"Net::Twitter::Lite/0.09002_03 (Perl)".
+"Net::Twitter::Lite/0.10000 (Perl)".
 
 =item source
 
